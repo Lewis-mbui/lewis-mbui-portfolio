44065684a90b5e850daebf93235c97206be6e7b3
Lewis-mbui <lewmugo14479@gmail.com>
2026-05-12 07:16:53 +0300

    update intro message

--- src/components/IntroSection.tsx
+++ src/components/IntroSection.tsx
@@ -1,4 +1,4 @@
-import PrimaryNav from "./PrimaryNav"
+import PrimaryNav from "./PrimaryNav";
 
 const IntroSection = () => {
   return (
@@ -6,33 +6,29 @@ const IntroSection = () => {
       <div className="intro__wrapper | wrapper grid">
         <PrimaryNav />
         <picture className="profile-container">
-          <source
-            media="(min-width: 900px)"
-            srcSet="/lewis-desktop.webp"
-          />
-          <source
-            media="(min-width: 720px)"
-            srcSet="/lewis-tablet.webp"
-          />
-          <img
-            src="/lewis-mobile.webp"
-            alt="Profile photo"
-          />
+          <source media="(min-width: 900px)" srcSet="/lewis-desktop.webp" />
+          <source media="(min-width: 720px)" srcSet="/lewis-tablet.webp" />
+          <img src="/lewis-mobile.webp" alt="Profile photo" />
         </picture>
 
         <div className="intro__content | grid">
           <h1 className="intro__header | heading-xl">
-            Nice to <br className="intro__break" />meet you! I'm <span className="decorate">Lewis Mbui</span>.
+            Nice to <br className="intro__break" />
+            meet you! I'm <span className="decorate">Lewis Mbui</span>.
           </h1>
           <p>
-            Based in Kenya, I'm a front-end developer passionate about building
-            accessible web apps that users love.
+            Based in Nairobi, Kenya - I'm a self-taught web developer passionate
+            about building clean, responsive and user-friendly web applications.
+            I enjoy turning ideas into real products while continuously
+            improving my skills in modern frontend and backend development.
           </p>
-          <a href="mailto: lewmugo14479@gmail.com" className="link">Contact me</a>
+          <a href="mailto: lewmugo14479@gmail.com" className="link">
+            Contact me
+          </a>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default IntroSection
+export default IntroSection;
